3eaab39cc456e86c2374a726a167dc9a30a643ae
საბა რევაზიშვილი <152292902+R3V4Z4@users.noreply.github.com>
2025-02-11 19:05:21 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,6 @@ let divAccArray = document.querySelectorAll(".one-accordion");
 
 divAccArray.forEach(function (item) {
   item.addEventListener("click", function () {
-    // Close all other accordions
     divAccArray.forEach((accordion) => {
       if (accordion !== this) {
         accordion.classList.remove("active");
@@ -44,16 +43,23 @@ divAccArray.forEach(function (item) {
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.getElementById("burgerBar");
   const menu = document.querySelector(".header");
-  burger.addEventListener("click", function () {
-    burger.classList.toggle("isactive");
-    menu.classList.toggle("expand");
-  });
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+  function toggleMenu() {
+    const isExpanded = menu.classList.toggle("expand");
+    burger.classList.toggle("isactive", isExpanded);
+    overlay.style.display = isExpanded ? "block" : "none";
+    document.body.classList.toggle("no-scroll", isExpanded);
+  }
+
+  burger.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu); // Close menu when clicking outside
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   const subMenus = document.querySelectorAll(".sub-menu");
   const buttons = document.querySelectorAll(".sidebar ul a");
-
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       subMenus.forEach((menu) => (menu.style.height = "0px"));
@@ -79,5 +85,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
-
